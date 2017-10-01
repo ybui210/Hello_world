b6e326390e23f7f1ab0c112f160e7077059575f6
Evgeny Ilyushkin <contact@hgoc.eu>
2017-10-01 22:42:06 +0200

    Updated for Common Lisp

--- hello_world.lisp
+++ hello_world.lisp
@@ -1,1 +1,5 @@
-(print "Hello, world!")
+;; to run, use "sbcl --script hello_world.lisp", for example
+(write-line "Hello World")
+(quit)
+
+;; use (print "Hello, world!") to output the sting with the double-quotes and escape characters to serve as input
