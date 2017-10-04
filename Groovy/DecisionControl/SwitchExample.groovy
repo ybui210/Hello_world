68ce5787d94d12355d4d0ebc9d55d03f3bd944c5
Himanshu Aggarwal <haggcoder@gmail.com>
2017-10-04 17:59:32 +0530

    Create SwitchExample.groovy

---
+++ Groovy/DecisionControl/SwitchExample.groovy
@@ -0,0 +1,32 @@
+class Example { 
+   static void main(String[] args) { 
+      //initializing a local variable 
+      int a = 2
+		
+      //Evaluating the expression value 
+      switch(a) {            
+         //There is case statement defined for 4 cases 
+         // Each case statement section has a break condition to exit the loop 
+			
+         case 1: 
+            println("The value of a is One"); 
+            break; 
+         case 2: 
+            println("The value of a is Two"); 
+            break; 
+         case 3: 
+            println("The value of a is Three"); 
+            break; 
+         case 4: 
+            println("The value of a is Four"); 
+            break; 
+         default: 
+            println("The value is unknown"); 
+            break; 
+      }
+   }
+}
+
+/*
+Output: The value of a is Two
+*/
